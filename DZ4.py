af8a990787cd4302fb42b07b3833cdbe5b446f64
iassnaia <110601635+iassnaia@users.noreply.github.com>
2024-04-06 10:05:30 +0300

    Update DZ4.py

--- DZ4.py
+++ DZ4.py
@@ -1,49 +1,143 @@
-От lxml импорт HTML
-импорт запросов
-От pprint импорт pprint
-От pymongo импорт MongoClient
-pymongo pymongo.ошибки импорт DuplicateKeyError как dke
-
-url = 'https://lenta.ru/'
-= заголовки {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, как у Gecko) Chrome/101.0.4951.64 Safari/537.36'}
-
-запросы = ответ.получить(URL, заголовки=заголовки)
-
-html = dom.fromstring(ответ.текст)
-
-dom = items.xpath("//a[содержит(@class, 'card-mini')]")
-
-= list_items []
-
-элементы в элементе для:
-
-    item_info = {}
-
-    источник = 'lenta.ru '
-    item = name.xpath(".//span[содержит(@class, 'card-mini__title')]/text()")[0]
-    элемент + url = ссылка.xpath("./@href")[0]
-    item = date.xpath(".//time[@class='card-mini__date']/text()")
-
-    item_info['source'] = источник
-    item_info['name'] = name
-    item_info['link'] = ссылка
-    item_info['date'] = дата
-
-    Список элементов.добавить(item_info)
-
-# pprint(list_items)
+from lxml import html
+import requests
+from datetime import datetime, timedelta
+from datetime import date
+from pprint import pprint
+from pymongo import MongoClient
+from pymongo import errors
 
 
-клиент = MongoClient('127.0.0.1', 27017)
+header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
 
-клиент = db['news182']
+client = MongoClient('localhost',27017)
+db = client['news_db']  # указать имя базы данных, которая будет создана
+NewsMongo = db.News # создать объект, в которую складывается коллекция News
 
-Коллекция = db.news182_mongo
+def save_new_to_db(new):
+    # Функция записи в БД. Возвращает 1, если новость быда добавлена,
+    # и возвращает 0, если новость не добавлена (уже есть в БД)
+    try:
+        db.NewsMongo.insert_one(new)
+    except errors.DuplicateKeyError:
+        pass
+        return 0
+    else:
+        return 1
 
-попробовать:
-    Коллекция.insert_many(list_items)
-dke кроме:
-    print(f'Пытаемся добавить такой же элемент!..')
+def request_to_yandex():
+    # Фунцкия собирает новости yandex.ru
+    base_url = 'https://yandex.ru/news'
+    response = requests.get(base_url, headers=header)
 
-список = результат(коллекция.найти({}))
-pprint(результат)
+    dom = html.fromstring(response.text)
+
+    # Новости собираются из 12-ти разделов
+    items = dom.xpath(
+        """//div[@class='mg-grid__row mg-grid__row_gap_8 news-top-rubric-stories news-app__top'] \
+        //div[@class='mg-grid__col mg-grid__col_xs_4'] \
+        | //div[@class='mg-grid__row mg-grid__row_gap_8 news-top-rubric-stories news-app__top'] \
+        //div[@class='mg-grid__col mg-grid__col_xs_6'] \
+        """)
+
+    for item in items:
+        new = {}
+
+        # название источника
+        source = item.xpath(".//a/text()")
+        new['source'] = source[0].replace(u'\n', u'')
+
+        # наименование новости
+        name = item.xpath(".//h2[@class='news-card__title']/text()")
+        new['name'] = name[0].replace(u'\xa0', u' ')
+
+        #ссылка на новость
+        url = item.xpath(".//a/@href")
+        new['url'] = url[0]
+
+        # дата публикации
+        time_item = item.xpath(".//span[@class='mg-card-source__time']/text()")
+        time = time_item[0].split(' ')[-1]
+        time_v = time_item[0].split(' ')[0]
+        Previous_Date = date.today() - timedelta(1)
+
+        if time_v == 'вчера':
+            new['time'] = f'{Previous_Date} {time}'
+        else:
+            new['time'] = f'{date.today()} {time}'
+
+        save_new_to_db(new)
+
+
+def request_to_lenta():
+    # Фунцкия собирает новости lenta.ru
+    base_url = 'https://lenta.ru'
+    response = requests.get(base_url, headers=header)
+
+    dom = html.fromstring(response.text)
+
+    items = dom.xpath(
+        """
+        //div[@class='span8 js-main__content']//div[@class='span4'] \
+        /div[@class='first-item'] \
+        | //div[@class='span8 js-main__content']//div[@class='span4'] \
+        /div[@class='item']
+        """)
+
+    for item in items:
+        new = {}
+        name = item.xpath(".//a/text()")
+        url = item.xpath(".//a/@href")
+        new['source'] = 'lenta.ru '
+        time = item.xpath(".//time/text()")
+        new['time'] = f'{date.today()} {time[0]}'
+        new['name'] = name[0].replace(u'\xa0', u' ')
+        new['url'] = base_url + url[0]
+        save_new_to_db(new)
+
+
+def request_to_mail():
+    # Фунцкия собирает новости mail.ru
+    base_url = 'https://news.mail.ru/'
+    response = requests.get(base_url, headers=header)
+
+    dom = html.fromstring(response.text)
+
+    items = dom.xpath(
+        """
+        //div[@class='cols__inner']//span[@class='cell'] \
+        | //div[@class='cols__inner']//span[@class='list__text']
+        """)
+
+    for item in items:
+        new = {}
+        name = item.xpath(".//span/text()")
+        url = item.xpath(".//a/@href")[0]
+
+        date_format = '%Y-%m-%dT%H:%M:%S%z'
+
+        # Переход по новости для сбора информации
+        new_more = requests.get(url, headers=header)
+        dom_more = html.fromstring(new_more.text)
+        source = dom_more.xpath("//a[@class='link color_gray breadcrumbs__link'] \
+        //span[@class='link__text']/text()")
+        if source:
+            new['source'] = source[0]
+        else:
+            new['source'] = None
+
+        date = dom_more.xpath("//span[@class='note__text breadcrumbs__text js-ago']/@datetime")
+        if len(date) > 0:
+            date = datetime.strptime(date[0], date_format)
+            date = date.strftime('%Y-%m-%d %H:%M')
+            new['time'] = f'{date}'
+        else:
+            new['time'] = None
+
+        new['name'] = name[0].replace(u'\xa0', u' ')
+        new['url'] = url
+        save_new_to_db(new)
+
+request_to_yandex()
+request_to_lenta()
+request_to_mail()
