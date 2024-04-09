51ff5bf17257d1dfaf16c7c34a02c13e14d40639
iassnaia <110601635+iassnaia@users.noreply.github.com>
2024-04-09 21:33:28 +0300

    Update DZ4.py

--- DZ4.py
+++ DZ4.py
@@ -1,143 +1,94 @@
-from lxml import html
 import requests
-from datetime import datetime, timedelta
-from datetime import date
-from pprint import pprint
-from pymongo import MongoClient
-from pymongo import errors
+from lxml import html
+import csv
+import pandas as pd
+from fake_useragent import UserAgent 
+import re
+from urllib.parse import urljoin
 
+url_base = "https://ru.wikipedia.org"
+url = "https://ru.wikipedia.org/wiki/Список_городов_России_с_населением_более_100_тысяч_жителей"
 
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-        AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
+ua = UserAgent() 
+headers = {
+    "User-Agent": ua.firefox, 
+}
 
-client = MongoClient('localhost',27017)
-db = client['news_db']  # указать имя базы данных, которая будет создана
-NewsMongo = db.News # создать объект, в которую складывается коллекция News
-
-def save_new_to_db(new):
-    # Функция записи в БД. Возвращает 1, если новость быда добавлена,
-    # и возвращает 0, если новость не добавлена (уже есть в БД)
-    try:
-        db.NewsMongo.insert_one(new)
-    except errors.DuplicateKeyError:
-        pass
-        return 0
+try:
+    response = requests.get(url, headers = headers)
+    if response.status_code == 200:
+        print("Успешный запрос API по URL: ", response.url)
     else:
-        return 1
+        print("Запрос API отклонен с кодом состояния:", response.status_code)
 
-def request_to_yandex():
-    # Фунцкия собирает новости yandex.ru
-    base_url = 'https://yandex.ru/news'
-    response = requests.get(base_url, headers=header)
+except requests.exceptions.RequestException as e:
+    print("Ошибка в осущественнии HTML запроса:", e)
 
-    dom = html.fromstring(response.text)
+try:
+    tree = html.fromstring(response.content)
+    print(tree)
+except html.etree.ParserError as e:
+    print("Ошибка в парсинге HTML содержимого:", e)
 
-    # Новости собираются из 12-ти разделов
-    items = dom.xpath(
-        """//div[@class='mg-grid__row mg-grid__row_gap_8 news-top-rubric-stories news-app__top'] \
-        //div[@class='mg-grid__col mg-grid__col_xs_4'] \
-        | //div[@class='mg-grid__row mg-grid__row_gap_8 news-top-rubric-stories news-app__top'] \
-        //div[@class='mg-grid__col mg-grid__col_xs_6'] \
-        """)
+try:
+    table_rows = tree.xpath("//table[@class='wikitable sortable']/tbody/tr")
+    len(table_rows)
+    
+except IndexError as e:
+    print("Ошибка доступа к результату:", e)
 
-    for item in items:
-        new = {}
+except Exception as e:
+    print("Произошла непредвиденная ошибка:", e)
 
-        # название источника
-        source = item.xpath(".//a/text()")
-        new['source'] = source[0].replace(u'\n', u'')
+try:
+    years = table_rows[0].xpath("//th/span/text()")[2:]
+    print(years)
+    
+except IndexError as e:
+    print("Ошибка доступа к результату:", e)
 
-        # наименование новости
-        name = item.xpath(".//h2[@class='news-card__title']/text()")
-        new['name'] = name[0].replace(u'\xa0', u' ')
+except Exception as e:
+    print("Произошла непредвиденная ошибка:", e)
 
-        #ссылка на новость
-        url = item.xpath(".//a/@href")
-        new['url'] = url[0]
+years.insert(0, "Город")
+years.append("Ссылка на информацию о городе")
 
-        # дата публикации
-        time_item = item.xpath(".//span[@class='mg-card-source__time']/text()")
-        time = time_item[0].split(' ')[-1]
-        time_v = time_item[0].split(' ')[0]
-        Previous_Date = date.today() - timedelta(1)
+df = pd.DataFrame(columns = years)
+print(years)
 
-        if time_v == 'вчера':
-            new['time'] = f'{Previous_Date} {time}'
-        else:
-            new['time'] = f'{date.today()} {time}'
+# парсинг таблицы
+try:
+    for row in table_rows[2:]:
+        # Работа с каждой ячейкой таблицы отдельно
+        cells = row.xpath('.//td|.//th')[2:]
+        
+        # Если в ячейке таблицы значения нет, то возвращаем None 
+        row_data = [cell.text_content().strip() if cell.text_content().strip() else None for cell in cells]
+        
+        # Если в ячейке таблицы прочерк, то замещаем его на None
+        row_data = [None if item == '—' else item for item in row_data]
+        
+        # убираем референсные ссылки на источники статистических данных
+        row_data = [re.sub("\[[0-9]+\]", '', s) if s is not None else None for s in row_data]
+        
+        # получаем ссылку на информацию о городе
+        city_wiki_ref = urljoin(url_base, row.xpath(".//td//a/@href")[0])
+        
+        # Объединяем данные из строки в итоговую таблицу
+        temp_df = pd.DataFrame(data = row_data)
+        temp_df = temp_df.transpose()
+        temp_df.columns = years[:-1]
+        temp_df["Ссылка на информацию о городе"] = city_wiki_ref
+        df = pd.concat([df, temp_df])
+        #print(temp_df)
+    
+except IndexError as e:
+    print("Ошибка доступа к результату:", e)
 
-        save_new_to_db(new)
+except Exception as e:
+    print("Произошла непредвиденная ошибка:", e)
 
+# Смотрим на готовую итоговую таблицу
+df
 
-def request_to_lenta():
-    # Фунцкия собирает новости lenta.ru
-    base_url = 'https://lenta.ru'
-    response = requests.get(base_url, headers=header)
-
-    dom = html.fromstring(response.text)
-
-    items = dom.xpath(
-        """
-        //div[@class='span8 js-main__content']//div[@class='span4'] \
-        /div[@class='first-item'] \
-        | //div[@class='span8 js-main__content']//div[@class='span4'] \
-        /div[@class='item']
-        """)
-
-    for item in items:
-        new = {}
-        name = item.xpath(".//a/text()")
-        url = item.xpath(".//a/@href")
-        new['source'] = 'lenta.ru '
-        time = item.xpath(".//time/text()")
-        new['time'] = f'{date.today()} {time[0]}'
-        new['name'] = name[0].replace(u'\xa0', u' ')
-        new['url'] = base_url + url[0]
-        save_new_to_db(new)
-
-
-def request_to_mail():
-    # Фунцкия собирает новости mail.ru
-    base_url = 'https://news.mail.ru/'
-    response = requests.get(base_url, headers=header)
-
-    dom = html.fromstring(response.text)
-
-    items = dom.xpath(
-        """
-        //div[@class='cols__inner']//span[@class='cell'] \
-        | //div[@class='cols__inner']//span[@class='list__text']
-        """)
-
-    for item in items:
-        new = {}
-        name = item.xpath(".//span/text()")
-        url = item.xpath(".//a/@href")[0]
-
-        date_format = '%Y-%m-%dT%H:%M:%S%z'
-
-        # Переход по новости для сбора информации
-        new_more = requests.get(url, headers=header)
-        dom_more = html.fromstring(new_more.text)
-        source = dom_more.xpath("//a[@class='link color_gray breadcrumbs__link'] \
-        //span[@class='link__text']/text()")
-        if source:
-            new['source'] = source[0]
-        else:
-            new['source'] = None
-
-        date = dom_more.xpath("//span[@class='note__text breadcrumbs__text js-ago']/@datetime")
-        if len(date) > 0:
-            date = datetime.strptime(date[0], date_format)
-            date = date.strftime('%Y-%m-%d %H:%M')
-            new['time'] = f'{date}'
-        else:
-            new['time'] = None
-
-        new['name'] = name[0].replace(u'\xa0', u' ')
-        new['url'] = url
-        save_new_to_db(new)
-
-request_to_yandex()
-request_to_lenta()
-request_to_mail()
+df.to_csv('List of Russian cities population.csv', index=False)
