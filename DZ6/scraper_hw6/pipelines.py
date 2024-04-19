9f1b34893383c905d3122d0abfd6452de802ff84
iassnaia <110601635+iassnaia@users.noreply.github.com>
2024-04-19 14:53:24 +0300

    Create pipelines.py

---
+++ DZ6/scraper_hw6/pipelines.py
@@ -0,0 +1,14 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from scrapy.pipelines.images import ImagesPipeline
+import hashlib
+
+class ImageName(ImagesPipeline):
+    def file_path(self, request, response=None, info=None, *, item=None):
+        image_guid = hashlib.sha1(request.url.encode()).hexdigest()
+        return f"{item['book_name']} - {image_guid}.jpg"
